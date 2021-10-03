8d4ab2eff6208293ec6531df8400bc49f6c385dd
verilogen123 <90295699+verilogen123@users.noreply.github.com>
2021-10-03 01:17:43 -0700

    Test

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,8 +1,14 @@
-const b1 = document.querySelector("button");
+const b1 = document.getElementById("btn1");
+const tb1 = document.getElementById("tbox1");
 
 function giveAlert()
 {
     alert("Hello, world!");
+}
+
+function changeSize()
+{
+    tb1.style.fontSize = "24rem";
 }
 
 b1.addEventListener('click', giveAlert);
