dea3d2236f1725e4c67c9b0433cbd2ffb49dadee
verilogen123 <90295699+verilogen123@users.noreply.github.com>
2021-10-06 11:12:12 -0700

    Test

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,5 +1,5 @@
-const button1 = document.getElementById("btn1");
-const textarea1 = document.getElementById("tbox1");
+const button1 = document.getElementById("button1");
+const textarea1 = document.getElementById("textarea1");
 const fancyRadio = document.getElementById("fancyRadio");
 const boringRadio = document.getElementById("boringRadio");
 
@@ -15,7 +15,7 @@ function makeTextBig()
 
 function fancifyStyle()
 {
-    textarea1.style.fontWeight = "800";
+    textarea1.style.fontWeight = "900";
     textarea1.style.color = "blue";
     textarea1.style.textDecoration = "underline";
 }
